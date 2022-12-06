61c542a8d7861b7b2d67f77f1ec8cd93a42341e8
kamandkrg <kamandkargar@gmail.com>
2022-12-06 18:54:39 +0330

    make models for ticket

--- core/ticket/models.py
+++ core/ticket/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+
+class Ticket(models.Model):
+    status_choice = (
+        (0, 'در حال بررسی'),
+        (1, 'تکمیل شده')
+    )
+    title = models.TextField(max_length=128)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.SmallIntegerField(choices=status_choice, default=0)
+    unread = models.BooleanField()
+    create_time = models.DateTimeField(auto_now_add=True)
+    modify_time = models.DateTimeField(auto_now=True)
+
+
+class SubTicket(models.Model):
+    parent = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=512)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modify_time = models.DateTimeField(auto_now=True)
