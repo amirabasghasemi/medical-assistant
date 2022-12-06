edc4521559733d752a72f9f7b9290f19e65a7c83
kamandkrg <kamandkargar@gmail.com>
2022-12-06 21:25:57 +0330

    fix bug in model ticket

--- core/ticket/models.py
+++ core/ticket/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.db import models
 
 
@@ -9,7 +9,7 @@ class Ticket(models.Model):
         (1, 'تکمیل شده')
     )
     title = models.TextField(max_length=128)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     status = models.SmallIntegerField(choices=status_choice, default=0)
     unread = models.BooleanField()
     create_time = models.DateTimeField(auto_now_add=True)
@@ -18,7 +18,7 @@ class Ticket(models.Model):
 
 class SubTicket(models.Model):
     parent = models.ForeignKey(Ticket, on_delete=models.CASCADE)
-    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     text = models.TextField(max_length=512)
     created_time = models.DateTimeField(auto_now_add=True)
     modify_time = models.DateTimeField(auto_now=True)
