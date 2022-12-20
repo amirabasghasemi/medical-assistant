0544c2625aaef4bf6e4df591d3c351b7da37c473
kamandkrg <kamandkargar@gmail.com>
2022-12-20 21:41:08 +0330

    write model for analyze

--- core/analyze/models.py
+++ core/analyze/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class DiabetesModel(models.Model):
+    Diabetes_012 = models.PositiveSmallIntegerField()
+    HighBP = models.PositiveSmallIntegerField()
+    HighChol = models.PositiveSmallIntegerField()
+    CholCheck = models.PositiveSmallIntegerField()
+    BMI = models.PositiveSmallIntegerField()
+    Smoker = models.PositiveSmallIntegerField()
+    Stroke = models.PositiveSmallIntegerField()
+    HeartDiseaseorAttack = models.PositiveSmallIntegerField()
+    PhysActivity = models.PositiveSmallIntegerField()
+    Fruits = models.PositiveSmallIntegerField()
+    Veggies = models.PositiveSmallIntegerField()
+    HvyAlcoholConsump = models.PositiveSmallIntegerField()
+    AnyHealthcare = models.PositiveSmallIntegerField()
+    NoDocbcCost = models.PositiveSmallIntegerField()
+    GenHlth = models.PositiveSmallIntegerField()
+    MentHlth = models.PositiveSmallIntegerField()
+    PhysHlth = models.PositiveSmallIntegerField()
+    DiffWalk = models.PositiveSmallIntegerField()
+    Sex = models.PositiveSmallIntegerField()
+    Age = models.PositiveSmallIntegerField()
+    Education = models.PositiveSmallIntegerField()
+
