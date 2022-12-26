e33aca23709ab5900058168c92cf96fcb8ded966
kamandkrg <kamandkargar@gmail.com>
2022-12-26 16:20:17 +0330

    change views in ticket

--- core/ticket/views.py
+++ core/ticket/views.py
@@ -7,18 +7,11 @@ def ticket(request):
 
 
 def recent_tickets(request):
-    all_tickets = TicketModel.objects.all().order_by('-created_at')
-    return render(request, 'ticket/ticket.html', {'all_tickets': all_tickets})
+    all_tickets = TicketModel.objects.order_by('-created_time')
+    return render(request, 'dashboard/ticket.html', {'all_tickets': all_tickets})
 
 
 # def ticket_by_id(request, ticket_id):
 #     ticket = TicketModel.objects.get(pk=ticket_id)
 #     return render(request, 'ticket/ticket.html', {'ticket': ticket})
 
-
-def test(request):
-    return render(request, 'ticket/test.html')
-
-
-def experiment(request):
-    return render(request, 'ticket/experiment.html')
