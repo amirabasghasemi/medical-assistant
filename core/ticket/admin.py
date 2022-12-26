752b5df4bf981963adcfa70b7f7bdacd81e3fc26
kamandkrg <kamandkargar@gmail.com>
2022-12-26 16:20:33 +0330

    change admin panel for ticket

--- core/ticket/admin.py
+++ core/ticket/admin.py
@@ -3,9 +3,9 @@ from ticket.models import TicketModel
 
 
 class TicketAdmin(admin.ModelAdmin):
-    date_hierarchy = 'created_at'
-    list_filter = ('status', 'assignee')
-    list_display = ('id', 'title', 'status', 'assignee', 'description', 'updated_at')
+    date_hierarchy = 'created_time'
+    list_filter = ('status', )
+    list_display = ('id', 'title', 'status')
     search_fields = ['title', 'status']
 
 
