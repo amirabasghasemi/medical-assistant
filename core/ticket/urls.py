2cf121f26f0d71280c6cebb909c16ce8397cfa9f
kamandkrg <kamandkargar@gmail.com>
2022-12-28 01:32:02 +0330

    add path conversation

--- core/ticket/urls.py
+++ core/ticket/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from ticket.views import tickets
+from ticket.views import tickets, ticket_conversation
 
 urlpatterns = [
     path('', tickets, name="tickets"),
+    path('<int:pk>/', ticket_conversation, name="ticket-conversation"),
 ]
