a9c17b79e21c7846f0e077000f9cadb7559b38c3
kamandkrg <kamandkargar@gmail.com>
2022-12-28 01:30:59 +0330

    make forms for subticketmodel

--- core/ticket/forms.py
+++ core/ticket/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 from core import settings
-from ticket.models import TicketModel
+from ticket.models import TicketModel, SubTicketModel
 
 
 class TicketForm(forms.ModelForm):
@@ -22,3 +22,12 @@ class TicketForm(forms.ModelForm):
              'aria-label': 'With textarea',
              'placeholder': 'توضیحات مربوط به تیکت خودرادراین قسمت واردنمایید'}
         )
+
+class SubTicketForm(forms.ModelForm):
+    class Meta:
+        model = SubTicketModel
+        fields = ('text', )
+    def __init__(self, *args, **kwargs):
+        super(SubTicketForm, self).__init__(*args, **kwargs)
+        self.fields['text'].widget.attrs.update({'class':'w-75 rounded-4 textarea-input p-3',
+                                                 'placeholder': 'متن خود را وارد کنید...'})
