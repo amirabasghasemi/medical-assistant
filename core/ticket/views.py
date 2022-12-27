08e8f9306f234c1f32094bfeeaccbba9021e5668
kamandkrg <kamandkargar@gmail.com>
2022-12-27 23:20:12 +0330

    create view for list of ticket and create  new ticket

--- core/ticket/views.py
+++ core/ticket/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+
+from ticket.forms import TicketForm
 from ticket.models import TicketModel
 
 
-def ticket(request):
-    return render(request, 'ticket/ticket.html')
+def tickets(request):
+    if request.method == 'POST':
+        form = TicketForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.created_by = request.user
+            instance.change_unread(request.user)
+            form.save()
+        return redirect('tickets')
+    else:
+        form = TicketForm()
+        ticket = TicketModel.objects.order_by('-created_time')
+        paginator = Paginator(ticket, 2)
+        page_number = request.GET.get('page', 1)
 
-
-def recent_tickets(request):
-    all_tickets = TicketModel.objects.order_by('-created_time')
-    return render(request, 'dashboard/ticket.html', {'all_tickets': all_tickets})
+        all_tickets = paginator.get_page(page_number)
+        return render(request, 'dashboard/ticket.html', {'all_tickets': all_tickets, 'form': form})
 
 
 # def ticket_by_id(request, ticket_id):
