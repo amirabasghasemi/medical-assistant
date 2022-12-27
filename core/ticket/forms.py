1100ffa6bfb0f4dfe81f619f84e30b8018b7d79b
kamandkrg <kamandkargar@gmail.com>
2022-12-27 23:19:03 +0330

    make forms for add new ticket

--- core/ticket/forms.py
+++ core/ticket/forms.py
@@ -1,7 +1,24 @@
 from django import forms
 
 from core import settings
+from ticket.models import TicketModel
 
 
-class TicketForm(forms.Form):
-    text = forms.TextInput()
+class TicketForm(forms.ModelForm):
+
+    class Meta:
+        model = TicketModel
+        fields = ('title', 'text')
+
+    def __init__(self, *args, **kwargs):
+        super(TicketForm, self).__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update(
+            {'class': 'col-12 rounded-2 input',
+             'type': 'text',
+             'placeholder': 'موضوع تیکت خود را وارد کنید'}
+        )
+        self.fields['text'].widget.attrs.update(
+            {'class': 'form-control col-12 rounded-2',
+             'aria-label': 'With textarea',
+             'placeholder': 'توضیحات مربوط به تیکت خودرادراین قسمت واردنمایید'}
+        )
