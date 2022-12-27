6cd76198febda02a5b92e8ec4cf8f62a0b31287f
kamandkrg <kamandkargar@gmail.com>
2022-12-27 23:20:46 +0330

    change urls

--- core/ticket/urls.py
+++ core/ticket/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
-from ticket.views import ticket, recent_tickets
+from ticket.views import tickets
 
 urlpatterns = [
-    path('', ticket, name="ticket"),
-    path('recent/', recent_tickets, name="recent-tickets"),
-    # path('#recent-tickets/<int:ticket_id>', ticket_by_id, name='ticket_by_id'),
+    path('', tickets, name="tickets"),
 ]
