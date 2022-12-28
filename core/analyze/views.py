1b5c809ac67c582ed296c8ef2213aa202c76f676
kamandkrg <kamandkargar@gmail.com>
2022-12-28 22:18:27 +0330

    add view for test and results

--- core/analyze/views.py
+++ core/analyze/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def test(request):
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, 'dashboard/test.html')
+
+def results(request):
+    pass
+
+
+
