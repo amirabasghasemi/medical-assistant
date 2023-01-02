1c09f23c631e40bc5a758cefb8f9497bfabdc10f
kamandkrg <kamandkargar@gmail.com>
2023-01-02 12:46:36 +0330

    make view for diabetes test

--- core/analyze/views.py
+++ core/analyze/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from analyze.analyze import analyze_diabetes
+from analyze.forms import DiabetesForm
 
 
 def test(request):
     if request.method == "POST":
-        pass
+        form = DiabetesForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.save_model(int(form.cleaned_data.get('Age')), request.user)
+            result, positive, negative, Diabetes_Prediction = analyze_diabetes(
+                instance.HighBP, instance.HighChol, instance.CholCheck, instance.BMI, instance.Smoker, instance.Stroke,
+                instance.HeartDiseaseorAttack, instance.PhysActivity, instance.Fruits, instance.Veggies,
+                instance.HvyAlcoholConsump, instance.AnyHealthcare, instance.NoDocbcCost, instance.GenHlth,
+                instance.MentHlth, instance.PhysHlth, instance.DiffWalk, instance.Sex, instance.Age, instance.Education
+            )
+            instance.Diabetes_012 = Diabetes_Prediction
+            instance.result = result
+            form.save()
+            return redirect('test')
+        return redirect('test')
     else:
-        return render(request, 'dashboard/test.html')
+        form = DiabetesForm()
+        return render(request, 'dashboard/test.html', {'form': form})
 
 def results(request):
     pass
